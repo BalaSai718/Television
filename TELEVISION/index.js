5a4c36802d0f6eb1adf41929187ef4225ba5987b
Bala Sai <balasai718@gmail.com>
2021-10-16 00:50:43 +0530

    First Commit

---
+++ TELEVISION/index.js
@@ -0,0 +1,48 @@
+function fun(){
+    location.reload();
+}
+function fun1(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/8McTsOqeueE?autoplay=1;";
+}
+function fun2(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/2pr2oR60jyg?autoplay=1;";
+}
+function fun3(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/IKj_cj51_W0?autoplay=1&amp";
+}
+function fun4(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/f1r7ij2ehio?autoplay=1";
+}
+function fun5(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/G1rF6YEoprM?autoplay=1&amp";
+}
+function fun6(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/L_UPHsGR6fM?autoplay=1&amp";
+}
+function fun7(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/cns0xtRNYyk?autoplay=1&amp";
+}
+function fun8(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/9ayvL1GtqIc?autoplay=1&amp";
+}
+function fun9(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/5UvC9XEAL9k?autoplay=1&amp";
+}
+function fun10(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/ttqffyZ2qdE?autoplay=1&amp";
+}
+function fun11(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/h99SrJB7F5M?autoplay=1&amp";
+}
+function fun12(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/NXFR5Tx0D40?autoplay=1&amp";
+}
+function fun13(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/yDU1RcYt5vg?autoplay=1&amp";
+}
+function fun14(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/W9NyVYHtrIU?autoplay=1&amp";
+}
+function fun15(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/36YnV9STBqc?autoplay=1&amp";
+}
